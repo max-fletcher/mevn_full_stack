fec6cd335e316c19a6c409acdc0a9fd22bddecd8
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2022-06-21 01:16:27 +0600

    Added a reset method. Form reset fucks things up. Take ideas from the method.

--- client/src/views/AddPostView.vue
+++ client/src/views/AddPostView.vue
@@ -8,18 +8,19 @@
                   <v-form ref="form" class="pa-5" @submit.prevent="submitForm" enctype="multipart/form-data">
                      <!-- :rules="title_rules" -->
                      <!-- :error="title_error" :error-messages="error_messages.title" -->
-                     <v-text-field label="Title" prepend-icon="mdi-format-title" v-model="post.title" :error="typeof(title_error) == 'string'" :error-messages="error_messages.title"></v-text-field>
+                     <v-text-field label="Title" prepend-icon="mdi-format-title" v-model="post.title" :error="title_error" :error-messages="error_title"></v-text-field>
                      <!-- <v-text-field label="Category" prepend-icon="mdi-format-list-text" v-model="post.category" :rules="rules"></v-text-field> -->
-                     <v-radio-group label="Category" v-model="post.category" prepend-icon="mdi-format-list-text" mandatory :error="category_error" :error-messages="error_messages.category">
+                     <v-radio-group label="Category" v-model="post.category" prepend-icon="mdi-format-list-text" mandatory :error="category_error" :error-messages="error_category">
                         <v-radio label="Active" value="active"></v-radio>
                         <v-radio label="Inactive" value="inactive" ></v-radio>
                         <v-radio label="Inactive" value="sadawdawd" ></v-radio>
                      </v-radio-group>
 
-                     <v-textarea label="Description" prepend-icon="mdi-clipboard-text-outline" v-model="post.description" auto-grow rows="2" :error="description_error" :error-messages="error_messages.description"></v-textarea>
+                     <v-textarea label="Description" prepend-icon="mdi-clipboard-text-outline" v-model="post.description" auto-grow rows="2" :error="description_error" :error-messages="error_description"></v-textarea>
                      <!-- :rules="rules" -->
-                     <v-file-input label="Select Image" @change="selectFile" ref="file" show-size :error="file_error" :error-messages="error_messages.file"></v-file-input>
+                     <v-file-input label="Select Image" @change="selectFile" ref="file" show-size :error="file_error" :error-messages="error_file"></v-file-input>
                      <v-btn type="submit" class="mt-3" color="primary"> Add Post </v-btn>
+                     <v-btn @click="reset" class="mt-3" color="primary"> Reset </v-btn>
                   </v-form>
                </v-card>
             </v-col>
@@ -35,7 +36,11 @@
       data() {
          return {
             // rules: [(value) => !!value || "This field is required !"],
-            error_messages:[],
+            error_title: "",
+            error_category: "",
+            error_description: "",
+            error_file: "",
+
             title_error: false,
             category_error: false,
             description_error: false,
@@ -65,12 +70,13 @@
 
       methods: {
          selectFile(file){
-            this.post.image = file
+            this.file_error = false
 
-            console.log(this.post.image)
-            
+            console.log("messag" ,this.post)
          },
+
          async submitForm(){
+            console.log(this.post)
             if(this.$refs.form.validate()){
                const formData = new FormData()
                formData.append('title', this.post.title)
@@ -89,22 +95,22 @@
                catch(error){
                   this.core_error_message = error.message
                   if(error.response.data.error.errors.find( ({ field }) => field === 'title' )){
-                     this.error_messages.title = error.response.data.error.errors.find( ({ field }) => field === 'title' ).message
+                     this.error_title = error.response.data.error.errors.find( ({ field }) => field === 'title' ).message
                      this.title_error = true
                   }
                   if(error.response.data.error.errors.find( ({ field }) => field === 'category' )){
-                     this.error_messages.category = error.response.data.error.errors.find( ({ field }) => field === 'category' ).message
+                     this.error_category = error.response.data.error.errors.find( ({ field }) => field === 'category' ).message
                      this.category_error = true
                   }
                   if(error.response.data.error.errors.find( ({ field }) => field === 'description' )){
-                     this.error_messages.description = error.response.data.error.errors.find( ({ field }) => field === 'description' ).message
+                     this.error_description = error.response.data.error.errors.find( ({ field }) => field === 'description' ).message
                      this.description_error = true
                   }
                   if(error.response.data.error.errors.find( ({ field }) => field === 'file' )){
-                     this.error_messages.file = error.response.data.error.errors.find( ({ field }) => field === 'file' ).message
+                     this.error_file = error.response.data.error.errors.find( ({ field }) => field === 'file' ).message
                      this.file_error = true
                   }
-                  console.log(this.error_messages)
+                  console.log(this.error_title, this.title_error)
                   // console.log(this.error_messages.title)
                   // console.log(error.response.data.error.errors)
                }
@@ -112,6 +118,26 @@
             else{
                this.$refs.form.validate()
             }
+         },
+
+         reset(){
+            console.log("asdawda")
+
+            this.post.title = ""
+            this.post.category = ""
+            this.post.description = ""
+            this.post.image = ""
+
+            this.error_title = ""
+            this.title_error = false
+            this.error_category= ""
+            this.category_error = false
+            this.error_description = ""
+            this.description_error = false
+            this.error_file = ""
+            this.file_error = false
+
+            console.log(this.error_title, this.title_error)
          }
       },
 
