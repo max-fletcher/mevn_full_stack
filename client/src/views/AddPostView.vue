72c9d3bf4a3b38f6b3996d3ce3fbd99ec5514e92
max-fletcher <amar.naam.machine@gmail.com>
2022-06-21 16:04:19 +0600

    backend validation works perfectly. Dicided to not implement frontend validation(maybe later)/

--- client/src/views/AddPostView.vue
+++ client/src/views/AddPostView.vue
@@ -1,148 +1,229 @@
 <template>
-      <v-container>
-         <v-row no-gutters>
-            <v-col sm="10" class="mx-auto">
-               <v-card class="pa-5">
-                  <v-card-title> Add New Post </v-card-title>
-                  <v-divider></v-divider>
-                  <v-form ref="form" class="pa-5" @submit.prevent="submitForm" enctype="multipart/form-data">
-                     <!-- :rules="title_rules" -->
-                     <!-- :error="title_error" :error-messages="error_messages.title" -->
-                     <v-text-field label="Title" prepend-icon="mdi-format-title" v-model="post.title" :error="title_error" :error-messages="error_title"></v-text-field>
-                     <!-- <v-text-field label="Category" prepend-icon="mdi-format-list-text" v-model="post.category" :rules="rules"></v-text-field> -->
-                     <v-radio-group label="Category" v-model="post.category" prepend-icon="mdi-format-list-text" mandatory :error="category_error" :error-messages="error_category">
-                        <v-radio label="Active" value="active"></v-radio>
-                        <v-radio label="Inactive" value="inactive" ></v-radio>
-                        <v-radio label="Inactive" value="sadawdawd" ></v-radio>
-                     </v-radio-group>
+   <v-container>
+      <v-row no-gutters>
+         <v-col sm="10" class="mx-auto">
+            <v-card class="pa-5">
+               <v-card-title> Add New Post </v-card-title>
+               <v-divider></v-divider>
+               <v-form
+                  ref="form"
+                  class="pa-5"
+                  @submit.prevent="submitForm"
+                  enctype="multipart/form-data"
+                  lazy-validation
+               >
+                  <!-- :rules="title_rules" -->
+                  <!-- :error="title_error" :error-messages="error_messages.title" -->
+                  <v-text-field
+                     label="Title"
+                     prepend-icon="mdi-format-title"
+                     v-model="post.title"
+                     :error-messages="title_error_message"
+                     @keyup="reset_title"
+                  ></v-text-field>
+                  <!-- <v-text-field label="Category" prepend-icon="mdi-format-list-text" v-model="post.category" :rules="rules"></v-text-field> -->
+                  <v-radio-group
+                     label="Category"
+                     v-model="post.category"
+                     prepend-icon="mdi-format-list-text"
+                     mandatory
+                     :error-messages="category_error_message"
+                     @change="reset_category"
+                  >
+                     <v-radio label="Active" value="active"></v-radio>
+                     <v-radio label="Inactive" value="inactive"></v-radio>
+                     <v-radio label="Inactive" value="sadawdawd"></v-radio>
+                  </v-radio-group>
 
-                     <v-textarea label="Description" prepend-icon="mdi-clipboard-text-outline" v-model="post.description" auto-grow rows="2" :error="description_error" :error-messages="error_description"></v-textarea>
-                     <!-- :rules="rules" -->
-                     <v-file-input label="Select Image" @change="selectFile" ref="file" show-size :error="file_error" :error-messages="error_file"></v-file-input>
-                     <v-btn type="submit" class="mt-3" color="primary"> Add Post </v-btn>
-                     <v-btn @click="reset" class="mt-3" color="primary"> Reset </v-btn>
-                  </v-form>
-               </v-card>
-            </v-col>
-         </v-row>
+                  <v-textarea
+                     label="Description"
+                     prepend-icon="mdi-clipboard-text-outline"
+                     v-model="post.description"
+                     auto-grow
+                     rows="2"
+                     :error-messages="description_error_message"
+                     @keyup="reset_description"
+                  ></v-textarea>
+                  <!-- :rules="rules" -->
+                  <v-file-input
+                     label="Select Image"
+                     @change="selectFile"
+                     ref="file"
+                     show-size
+                     :error-messages="file_error_message"
+                  ></v-file-input>
+                  <v-btn type="submit" class="mt-3" color="primary">
+                     Add Post
+                  </v-btn>
+                  <v-btn @click="reset_file" class="mt-3" color="primary">
+                     Reset
+                  </v-btn>
+               </v-form>
+            </v-card>
+         </v-col>
+      </v-row>
    </v-container>
 </template>
 
 <script>
-   import API from "../api"
-   export default {
-      name: 'AddPost',
+import API from "../api";
+export default {
+   name: "AddPost",
 
-      data() {
-         return {
-            // rules: [(value) => !!value || "This field is required !"],
-            error_title: "",
-            error_category: "",
-            error_description: "",
-            error_file: "",
+   data() {
+      return {
+         // rules: [(value) => !!value || "This field is required !"],
+         core_error_message: null,
+         
+         title_error_message: "",
+         category_error_message: "",
+         description_error_message: "",
+         file_error_message: "",
 
-            title_error: false,
-            category_error: false,
-            description_error: false,
-            file_error: false,
+         post: {
+            title: "",
+            category: "",
+            description: "",
+            image: "",
+         },
 
-            core_error_message: null,
+         // title_rules: [
+         //    (v) => !!v || "Title is required",
+         //    (v) =>
+         //       (v && v.length >= 3) ||
+         //       "Title must be greater than 3 characters",
+         //    (v) =>
+         //       (v && v.length <= 100) ||
+         //       "Title must be less than 100 characters",
+         // ],
+         // description_rules: [
+         //    (v) => !!v || "Description is required",
+         //    (v) =>
+         //       (v && v.length >= 3) ||
+         //       "Title must be greater than 3 characters",
+         //    (v) =>
+         //       (v && v.length <= 1000) ||
+         //       "Title must be less than 1000 characters",
+         // ],
+         // category_rules: [
+         //    (v) => !!v || "Description is required",
+         //    (v) =>
+         //       (v && v != 'active') || (v && v != 'inactive')
+         // ],
+      };
+   },
 
-            post: {
-               title: "",
-               category: "",
-               description: "",
-               image: "",
-            },
+   methods: {
+      selectFile(file) {
+         this.reset_file()
+         this.file_error = false;
+         console.log("messag", this.post);
+      },
 
-            title_rules: [
-               (v) => !!v || "Title is required",
-               (v) => (v && v.length >= 3) || 'Title must be greater than 3 characters',
-               (v) => (v && v.length <= 100) || 'Title must be less than 100 characters',
-            ],
-            description_rules: [
-               (v) => !!v || "Description is required",
-               (v) => (v && v.length >= 3) || 'Title must be greater than 3 characters',
-               (v) => (v && v.length <= 1000) || 'Title must be less than 1000 characters',
-            ],
+      async submitForm() {
+         console.log(this.post);
+         if (this.$refs.form.validate()) {
+            const formData = new FormData();
+            formData.append("title", this.post.title);
+            formData.append("category", this.post.category);
+            formData.append("description", this.post.description);
+            formData.append("file", this.post.image);
+
+            // for (const value of formData.values()) {
+            //    console.log(value);
+            // }
+
+            try {
+               const response = await API.storePost(formData);
+               this.$router.push({
+                  name: "Home",
+                  params: { message: "Post Created Successfully!" },
+               });
+            } catch (error) {
+               this.core_error_message = error.message;
+               if (
+                  error.response.data.error.errors.find(
+                     ({ field }) => field === "title"
+                  )
+               ) {
+                  this.title_error_message = error.response.data.error.errors.find(
+                     ({ field }) => field === "title"
+                  ).message;
+                  this.title_error = true;
+               }
+               if (
+                  error.response.data.error.errors.find(
+                     ({ field }) => field === "category"
+                  )
+               ) {
+                  this.category_error_message = error.response.data.error.errors.find(
+                     ({ field }) => field === "category"
+                  ).message;
+                  this.category_error = true;
+               }
+               if (
+                  error.response.data.error.errors.find(
+                     ({ field }) => field === "description"
+                  )
+               ) {
+                  this.description_error_message =
+                     error.response.data.error.errors.find(
+                        ({ field }) => field === "description"
+                     ).message;
+                  this.description_error = true;
+               }
+               if (
+                  error.response.data.error.errors.find(
+                     ({ field }) => field === "file"
+                  )
+               ) {
+                  this.file_error_message = error.response.data.error.errors.find(
+                     ({ field }) => field === "file"
+                  ).message;
+                  this.file_error = true;
+               }
+               console.log(this.title_error_message, this.title_error);
+               // console.log(this.error_messages.title)
+               // console.log(error.response.data.error.errors)
+            }
+         } else {
+            this.$refs.form.validate();
          }
       },
 
-      methods: {
-         selectFile(file){
-            this.file_error = false
-
-            console.log("messag" ,this.post)
-         },
-
-         async submitForm(){
-            console.log(this.post)
-            if(this.$refs.form.validate()){
-               const formData = new FormData()
-               formData.append('title', this.post.title)
-               formData.append('category', this.post.category)
-               formData.append('description', this.post.description)
-               formData.append('file', this.post.image)
-
-               // for (const value of formData.values()) {
-               //    console.log(value);
-               // }
-
-               try{
-                  const response = await API.storePost(formData)
-                  this.$router.push({ name: "Home", params: { message: "Post Created Successfully!" }})
-               }
-               catch(error){
-                  this.core_error_message = error.message
-                  if(error.response.data.error.errors.find( ({ field }) => field === 'title' )){
-                     this.error_title = error.response.data.error.errors.find( ({ field }) => field === 'title' ).message
-                     this.title_error = true
-                  }
-                  if(error.response.data.error.errors.find( ({ field }) => field === 'category' )){
-                     this.error_category = error.response.data.error.errors.find( ({ field }) => field === 'category' ).message
-                     this.category_error = true
-                  }
-                  if(error.response.data.error.errors.find( ({ field }) => field === 'description' )){
-                     this.error_description = error.response.data.error.errors.find( ({ field }) => field === 'description' ).message
-                     this.description_error = true
-                  }
-                  if(error.response.data.error.errors.find( ({ field }) => field === 'file' )){
-                     this.error_file = error.response.data.error.errors.find( ({ field }) => field === 'file' ).message
-                     this.file_error = true
-                  }
-                  console.log(this.error_title, this.title_error)
-                  // console.log(this.error_messages.title)
-                  // console.log(error.response.data.error.errors)
-               }
-            }
-            else{
-               this.$refs.form.validate()
-            }
-         },
-
-         reset(){
-            console.log("asdawda")
-
-            this.post.title = ""
-            this.post.category = ""
-            this.post.description = ""
-            this.post.image = ""
-
-            this.error_title = ""
-            this.title_error = false
-            this.error_category= ""
-            this.category_error = false
-            this.error_description = ""
-            this.description_error = false
-            this.error_file = ""
-            this.file_error = false
-
-            console.log(this.error_title, this.title_error)
-         }
+      reset_title() {
+         this.title_error_message = ""
       },
 
-      components: {
-         // HelloWorld,
+      reset_category() {
+         this.category_error_message = ""
       },
-   }
+
+      reset_description() {
+         this.description_error_message = ""
+      },
+
+      reset_file() {
+         this.file_error_message = ""
+      },
+
+      reset() {
+         // this.title_error = false;
+         // this.category_error = false;
+         // this.description_error = false;
+         // this.file_error = false;
+
+         this.title_error_message = "";
+         this.category_error_message = "";
+         this.description_error_message = "";
+         this.file_error_message = "";
+
+         // console.log(this.title_error_message, this.title_error);
+      },
+   },
+
+   components: {
+      // HelloWorld,
+   },
+};
 </script>
